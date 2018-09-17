2376ad246ca84a28ccf199b811a43ff6ec851ded
alvinfrancis <alvin.francis.dumalus@gmail.com>
2018-09-17 16:39:09 +0800

    Add codox plugin

--- project.clj
+++ project.clj
@@ -5,10 +5,14 @@
             :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :source-paths ["src/clj"]
+  :plugins [[lein-codox "0.10.4"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [io.opentracing/opentracing-api "0.31.0"]
                  [io.opentracing/opentracing-noop "0.31.0"]
                  [io.opentracing/opentracing-util "0.31.0"]
                  [ring "1.6.3"]]
+  :codox {:output-path "codox"
+          :metadata    {:doc/format :markdown}
+          :source-uri  "https://github.com/alvinfrancis/opentracing-clj/blob/{version}/{filepath}#L{line}"}
   :profiles {:test {:dependencies [[io.opentracing/opentracing-mock "0.31.0"]
                                    [ring/ring-mock "0.3.2"]]}})
