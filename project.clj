dc1c1742e746aca0333b6e5d2b72f778fef9b1c3
alvinfrancis <alvin.francis.dumalus@gmail.com>
2021-02-22 23:40:50 +0100

    Set version to 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject opentracing-clj "0.2.2"
+(defproject opentracing-clj "0.2.3-SNAPSHOT"
   :description "Opentracing API support for Clojure built on top of opentracing-java."
   :url "https://github.com/alvinfrancis/opentracing-clj"
   :license {:name         "Eclipse Public License"
